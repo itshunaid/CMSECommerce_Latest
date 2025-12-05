8554d038dcbd7f2bf9b5f8772e043c8be065d95b
inhush00 <hunaid.hussain@ingrammicro.com>
2025-12-05 16:54:40 +0530

    Upgraded vepi deployment file

--- CMSECommerce/Migrations/20251205083043_UpdateDatabase.Designer.cs
+++ CMSECommerce/Migrations/20251205083043_UpdateDatabase.Designer.cs
@@ -11,8 +11,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CMSECommerce.Migrations
 {
     [DbContext(typeof(DataContext))]
-    [Migration("20251205070754_Initial")]
-    partial class Initial
+    [Migration("20251205083043_UpdateDatabase")]
+    partial class UpdateDatabase
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
